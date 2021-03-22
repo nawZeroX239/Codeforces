d0a8cb93415558f4f076405b51451427986c191d
naw11235 <naw11235@cs.washington.edu>
2021-03-21 22:13:34 -0700

    added more comments and it now prints the top 10 tags

--- main.py
+++ main.py
@@ -1,20 +1,22 @@
 from selenium import webdriver
-from selenium.webdriver.common.keys import Keys
-import time
+from selenium.common.exceptions import NoSuchElementException
 
 driver = webdriver.Safari()
 
+# returns a mapping of problem tags to the number of times(frequency) it appears in a problem for a given rating
 def collect(rating_num):
+    # dictionary
     freq = {}
-    # https://codeforces.com/problemset/page/1?tags=1600-&order=BY_RATING_ASC
-    driver.get("https://codeforces.com/problemset?tags="+str(rating_num)+"-&order=BY_RATING_ASC")
-    lo = 1
-    hi = 1
-    for page_num in driver.find_element_by_class_name("pagination").find_elements_by_class_name("page-index"):
-        hi = max(hi, int(page_num.get_attribute("pageindex")))
 
-    # print("lo: " + str(lo) + " hi: " + str(hi))
-    for page_num in range(1, hi+1):
+    # base url
+    driver.get("https://codeforces.com/problemset?tags="+str(rating_num)+"-&order=BY_RATING_ASC")
+    last = 1
+    # determine the page number for last page
+    for page_num in driver.find_element_by_class_name("pagination").find_elements_by_class_name("page-index"):
+        last = max(last, int(page_num.get_attribute("pageindex")))
+
+    # loads pages until it reaches the last page that contains the problems with the given rating number
+    for page_num in range(1, last+1):
         url = "https://codeforces.com/problemset/page/"+str(page_num)+"?tags="+str(rating_num)+"-&order=BY_RATING_ASC"
         driver.get(url)
         # print("navigating to page: " + url)
@@ -41,23 +43,27 @@ def collect(rating_num):
             if int(rating) > rating_num:
                 return freq
 
-
-            # needs to handle when solved count is zero or doesn't exist
+            # exception is thrown when the number of participants solve is empty
             solved_count = ""
             try:
                 solved_count = cols[4].find_element_by_tag_name("a").get_attribute("innerText")[2:]
-            except:
+            except NoSuchElementException:
                 solved_count = ""
-
             # print(cols[0].find_element_by_tag_name("a").get_attribute("innerHTML").strip() + " " + title + " "
             #       + " ".join(tags) + " " + rating + " " )
     return freq
 
-
+# pass the rating number to collect method
 freq = collect(1500)
-#  = {"implementation":133, "greedy":95, "math":88, "brute force": 56}
+
+# dummy value
+# freq = {"implementation":133, "greedy":95, "math":88, "brute force": 56}
+
+# sort by increasing value in a dictionary
 sorted_freq = sorted(freq.items(), key=lambda kv : kv[1], reverse=True)
-# print(sorted_freq)
-for tup in sorted_freq:
-    print(tup)
+
+# prints top 10 tags with the count for each tag
+for i in range(min(10, len(sorted_freq))):
+    print(sorted_freq[i][0] + ' ' + str(sorted_freq[i][1]))
+
 driver.close()
